bf476809ebed678d151584968623db6d079f2c69
Rodymus <robertogonzalez.ar@gmail.com>
2022-05-14 12:07:14 -0300

    backend en java arrancando
    Se arranco con el backend en java

---
+++ PI-RCG-Backend/src/main/java/com/portafolioRCG/RCG/RcgApplication.java
@@ -0,0 +1,13 @@
+package com.portafolioRCG.RCG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcgApplication.class, args);
+	}
+
+}
---
+++ PI-RCG-Backend/src/test/java/com/portafolioRCG/RCG/RcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolioRCG.RCG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
